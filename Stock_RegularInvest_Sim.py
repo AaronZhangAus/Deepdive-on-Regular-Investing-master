454d0a806fbdf111ce8a35219089a18cce042377
Aaron Zhang <AaronZhangAus@users.noreply.github.com>
2021-08-25 10:34:55 +0930

    updating readme

--- Stock_RegularInvest_Sim.py
+++ Stock_RegularInvest_Sim.py
@@ -2,7 +2,7 @@ from RegularInvest import RegularInvest
 import pandas_datareader.data as web
 
 for i in range(1):
-    exp1 = RegularInvest(weeks=52, stock="TSM", invest_amount=100, beta=3000)
+    exp1 = RegularInvest(weeks=52, stock="fb", invest_amount=100, beta=3000)
     #exp1.populate_coin_price()
     #exp1.populate_coin_price_with_sin()
     exp1.populate_stock_price()
